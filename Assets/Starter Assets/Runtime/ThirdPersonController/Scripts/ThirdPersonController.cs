806520a8e143627d166ff260632c30f6f96e79ab
PinRong1014 <113153001@gas.fgu.edu.tw>
2025-11-14 09:16:37 +0800

    1007第六次同步
    人變成殭屍走路
    子彈設置

--- Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -14,6 +14,9 @@ namespace StarterAssets
 #endif
     public class ThirdPersonController : MonoBehaviour
     {
+        public Transform 發射點;
+        public GameObject 子彈;
+
         [Header("Player")]
         [Tooltip("Move speed of the character in m/s")]
         public float MoveSpeed = 2.0f;
@@ -394,11 +397,13 @@ namespace StarterAssets
             }
         }
 
-
-        public void startFiring()
+        public void 發射子彈事件()
         { 
             Debug.Log("開始射擊");
-            _controller.height = 1.7f;
+            GameObject bb = Instantiate(子彈, 發射點.position, Quaternion.identity);
+            bb.GetComponent<Rigidbody>().linearVelocity = Vector3.forward * Time.deltaTime * 100;
+            Destroy(bb, 3f);
+            //_controller.height = 1.7f;
         }
         public void endFiring()
         {
