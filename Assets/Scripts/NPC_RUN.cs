263d9475fcee6384595ef627354bc841d66afb5d
PinRong1014 <113153001@gas.fgu.edu.tw>
2026-01-12 19:58:12 +0800

    成果
    攻擊範圍、小修改

--- Assets/Scripts/NPC_RUN.cs
+++ Assets/Scripts/NPC_RUN.cs
@@ -9,23 +9,64 @@ public class NPC_RUN : MonoBehaviour
     public Transform 目標;
     public float 距離 = 0;
 
+    public GameObject 血條組件;
     public TextMeshPro 血量文字;
     public int 血量 = 100;
+    int 原始血量;
     public Transform 血條;
+    bool 開始攻擊 = false;
+    float 攻擊間距 = 2f;
+    float 下次可攻擊時間;
+    public float 攻擊距離 = 1.2f;
+
+
     void Start()
     {
         導航 = GetComponent<NavMeshAgent>();
         動畫器 = GetComponent<Animator>();
+        原始血量 = 血量;
+        血量文字.text = 血量.ToString();
+        導航.stoppingDistance = 攻擊距離;
+        目標 = null;
+        //if (目標 == null)
+        //{
+        //    目標 = GameObject.FindWithTag("Player").transform;
+        //}
     }
     void Update()
     {
-        if (目標 != null)
-        {
-            導航.SetDestination(目標.position);
-            距離 = Vector3.Distance(目標.position, this.transform.position);
-            if (距離 <= 3.1f) { 動畫器.SetBool("isWalk", false); }
-            else { 動畫器.SetBool("isWalk", true); }
-        }
+        //血條要對準攝影機
+        血條組件.transform.forward = Camera.main.transform.forward;
+        //if (目標 != null)
+        //{
+        //    導航.SetDestination(目標.position);
+        //    距離 = Vector3.Distance(目標.position, this.transform.position);
+        //    if(距離 <= 攻擊距離)   
+        //    { 
+        //        動畫器.SetBool("isWalk",false);
+        //        開始攻擊 = true;
+        //    }
+        //    else                       
+        //    { 
+        //        動畫器.SetBool("isWalk", true);
+        //        開始攻擊 = false;
+        //        //目標 = GameObject.FindWithTag("Player").transform;
+        //        導航.SetDestination(目標.position);
+        //    }
+        //if(開始攻擊)
+        //{
+        //    if (血量 <= 0) return;
+        //    if (Time.time >= 下次可攻擊時間)
+        //    {
+        //        動畫器.SetTrigger("isAttack");
+        //        下次可攻擊時間 = Time.time + 攻擊間距;
+        //    }
+        //}
+        //}
+        //else
+        //{
+        //    //目標 = GameObject.FindWithTag("Player").transform;
+        //}
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -34,9 +75,17 @@ public class NPC_RUN : MonoBehaviour
             Destroy(other.gameObject);
             血量--;
             血量文字.text = 血量.ToString();
-            float 血量比例 = (float)血量 / 100f;
+            float 血量比例 = (float)血量 / (float)原始血量;
             血條.localScale = new Vector3(血量比例, 1, 1);
-            if (血量 <= 0) { Destroy(this.gameObject); }
+            if (血量 <= 0)
+            {
+                動畫器.SetTrigger("isDead");
+                Destroy(this.gameObject, 3f);
+            }
+            else
+            {
+                動畫器.SetTrigger("isHit");
+            }
         }
     }
 }
--- Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/Starter Assets/Runtime/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 #if ENABLE_INPUT_SYSTEM 
 using UnityEngine.InputSystem;
 #endif
@@ -16,6 +17,13 @@ namespace StarterAssets
     {
         public Transform 發射點;
         public GameObject 子彈;
+        public int 子彈速度 = 100;
+
+        public TextMeshPro 血量文字;
+        public int 血量 = 100;
+        int 原始血量;
+        public Transform 血條;
+        public GameObject 血條組件;
 
         [Header("Player")]
         [Tooltip("Move speed of the character in m/s")]
@@ -153,6 +161,8 @@ namespace StarterAssets
             // reset our timeouts on start
             _jumpTimeoutDelta = JumpTimeout;
             _fallTimeoutDelta = FallTimeout;
+
+            原始血量 = 血量;
         }
 
         private void Update()
@@ -167,8 +177,30 @@ namespace StarterAssets
             {
                 _animator.SetTrigger("Fire");
             }
+            //血條要對準攝影機
+            血條組件.transform.forward = Camera.main.transform.forward;
         }
-
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "EnemyAttack")
+            {
+                //Destroy(other.gameObject);
+                血量--;
+                血量文字.text = 血量.ToString();
+                float 血量比例 = (float)血量 / (float)原始血量;
+                血條.localScale = new Vector3(血量比例, 1, 1);
+                if (血量 <= 0)
+                {
+                    _animator.SetTrigger("isDead");
+                    GameObject myPlayer = GameObject.Find("myPlayer");
+                    Destroy(myPlayer.gameObject, 3f);
+                }
+                else
+                {
+                    _animator.SetTrigger("isHit");
+                }
+            }
+        }
         private void LateUpdate()
         {
             CameraRotation();
@@ -408,13 +440,10 @@ namespace StarterAssets
                 方向 = (目標 - 發射點.position).normalized;
                 發射點.rotation = Quaternion.LookRotation(方向);
                 GameObject bb = Instantiate(子彈, 發射點.position, 發射點.rotation);
-                bb.GetComponent<Rigidbody>().linearVelocity = 方向 * Time.deltaTime * 100;
+                bb.GetComponent<Rigidbody>().linearVelocity = 方向 * Time.deltaTime * 子彈速度;
                 Destroy(bb, 3f);
             }
-            else
-            {
-                print("沒找到目標");
-            }
         }
+
     }
 }
